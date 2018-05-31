d228e51c6c9ba745dae511ea2fc93314787afaca
Abd El Rahman Hassan <abdelrahman.hsn@2loops.com>
2018-05-31 15:45:10 +0200

    make it better

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  protect_from_forgery with: :exception
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
