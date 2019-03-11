617379b160bb25521e316a72e209fb7b22628833
Abd El Rahman  HSN <abdelrahmanhsn1@gmail.com>
2019-03-11 22:09:04 +0200

    Add Image In Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
+# This Add Post In Blog
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' },
+                            default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
 end
